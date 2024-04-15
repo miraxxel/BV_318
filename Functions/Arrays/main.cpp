bd8f16da4928fd95d367b0cb68eec0e947441350
miraxxel <forremains@yandex.ru>
2024-04-15 21:26:27 +0300

    FillRand out of main

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -1,16 +1,9 @@
 ﻿//Arrays
-#include<iostream>
-using namespace std;
-
-#define tab "\t"
-#define delimiter "\n-----------------------------------------------------\n"
-
-const int ROWS = 3;	//количество строк
-const int COLS = 4; //количество элементов строки
-
-void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
-void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS, int minRand = 0, int maxRand = 100);
+/* Заголовочные файлы подключаются на место вызова, в отличии от других cpp файлов, 
+они просто сохраняются в проекте, подключение их - задача компоновщика */
+#include"stdafx.h" // Такое делается не во всех проектах
+#include"Constants.h" // Подключение заголовочного файла
+#include"FillRand.h"
 
 void Print(const int arr[], const int n);
 void Print(const double arr[], const int n);
@@ -95,54 +88,6 @@ void main()
 
 }
 
-void FillRand(int arr[], const int n, int minRand, int maxRand)
-{
-	if (maxRand < minRand)
-	{
-		int buffer = minRand;
-		minRand = maxRand;
-		maxRand = buffer;
-	}
-	//Заполнение массива случайными числами:
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = minRand + rand() % (maxRand - minRand);
-		//arr[i] = 50 + rand() % 50;
-		//Функция rand() генерирует псевдослучайное число в диапазоне от 0 до 32 767 (RAND_MAX)
-		//Это псевдослучайное число можно вывести на экран, сохранить в переменную, или элемент массива.
-	}
-}
-void FillRand(double arr[], const int n, int minRand, int maxRand)
-{
-	if (maxRand < minRand)
-	{
-		int buffer = minRand;
-		minRand = maxRand;
-		maxRand = buffer;
-	}
-	//Заполнение массива случайными числами:
-	minRand *= 100;
-	maxRand *= 100;
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = minRand + rand() % (maxRand - minRand);
-		arr[i] /= 100;
-		//arr[i] = 50 + rand() % 50;
-		//Функция rand() генерирует псевдослучайное число в диапазоне от 0 до 32 767 (RAND_MAX)
-		//Это псевдослучайное число можно вывести на экран, сохранить в переменную, или элемент массива.
-	}
-}
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS, int minRand, int maxRand)
-{
-	//Заполнение двумерного массива случайными числами:
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			arr[i][j] = rand() % 100;
-		}
-	}
-}
 
 void Print(const int arr[], const int n)
 {
---
+++ Functions/Arrays/stdafx.h
@@ -0,0 +1,6 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+// в этом файле то, что используетс€ во всем проекте (или почти во всЄм)
+// он же может быть precompilire header, ускор€ет процесс компил€цию
